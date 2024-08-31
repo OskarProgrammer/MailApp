462a64a12606376ec720730a15edbbc19e9f2738
Oskar <Zaden>
2024-08-31 15:27:16 +0200

    Creating navbar in the MailLayout.jsx

--- src/layouts/MailLayout.jsx
+++ src/layouts/MailLayout.jsx
@@ -1,20 +1,30 @@
-import { redirect } from "react-router-dom"
-import { getRequest } from "../API/requests"
+import { NavLink, redirect } from "react-router-dom"
+import { getRequest, getRequestId } from "../API/requests"
 
 export const MailLayout = () => {
 
     return (
         <div className="text-light">
-            Mail layout
+            <div className="navbar bg-light m-2 px-4 py-3 fs-5 rounded-pill">
+                <div className="col-lg-6">
+                    <NavLink to="/mail/" className="btn btn-outline-dark btn-lg">Home</NavLink>
+                </div>
+                <div className="col-lg-6 d-flex justify-content-end gap-3">
+                    <NavLink className="btn btn-outline-success btn-lg">Send message</NavLink>
+                    <NavLink to="/mail/logOut" className="btn btn-outline-danger btn-lg">Log Out</NavLink>
+                </div>
+            </div>
+
         </div>
     )
 }
 
 export const mailLayoutloader = async () => {
-    const {isLogged} = await getRequest("http://localhost:3000/currentUser/")
-    
+    const {id ,isLogged} = await getRequest("http://localhost:3000/currentUser/")
+    const currentUserData = await getRequestId("http://localhost:3000/users/", id)
+
     if (isLogged) {
-        return redirect("/mail/")
+        return currentUserData 
     }else{
         return redirect("/")
     }
