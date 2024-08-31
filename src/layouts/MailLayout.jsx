29325889adb418ff855fef8bd4fd50567719dc34
Oskar <Zaden>
2024-08-31 23:40:19 +0200

    Connecting new route to the navlink: '/mail/sentMessages'

--- src/layouts/MailLayout.jsx
+++ src/layouts/MailLayout.jsx
@@ -11,12 +11,12 @@ export const MailLayout = () => {
 
             <div className="mainSection text-dark fs-5 ms-4 me-5 my-3 d-flex justify-content-start gap-4 text-center">
                 <div className="col-lg-1 bg-light rounded d-flex flex-column py-3">
-                    <NavLink className="btn btn-outline-dark m-2"><i class="bi bi-chat-fill fs-2"/></NavLink>
+                    <NavLink to="/mail/sentMessages" className="btn btn-outline-dark m-2"><i class="bi bi-send fs-2"/></NavLink>
                     <NavLink className="btn btn-outline-dark m-2"><i class="bi bi-chat fs-2"/></NavLink>
                     <NavLink className="btn btn-outline-dark m-2"><i class="bi bi-people-fill fs-2"/></NavLink>
                     <NavLink className="btn btn-outline-dark m-2"><i class="bi bi-trash fs-2"/></NavLink>
                 </div>
-                <div className="col-lg-11 bg-light rounded">
+                <div className="col-lg-11 col-md-11 col-sm-10 col-10 bg-light rounded">
                     <Outlet/>
                 </div>
             </div>
