44b57d410273b98c479338add9ec837c31f896a3
Oskar <Zaden>
2024-08-31 15:41:11 +0200

    Creating NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom"
+
+export const NavBar = () => {
+    return(
+        <div className="navbar bg-light m-3 px-4 py-3 fs-5 rounded-pill">
+                <div className="col-lg-6">
+                    <NavLink to="/mail/" className="btn btn-outline-dark btn-lg">Home</NavLink>
+                </div>
+                <div className="col-lg-6 d-flex justify-content-end gap-3">
+                    <NavLink className="btn btn-outline-success btn-lg">Send message</NavLink>
+                    <NavLink to="/mail/logOut" className="btn btn-outline-danger btn-lg">Log Out</NavLink>
+                </div>
+        </div>
+    )
+}
