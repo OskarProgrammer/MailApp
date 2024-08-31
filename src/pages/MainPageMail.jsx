be91275a6239ec6a01aa2e3a022ce4ff29e8412b
Oskar <Zaden>
2024-08-31 22:16:20 +0200

    Changing the border of profile

--- src/pages/MainPageMail.jsx
+++ src/pages/MainPageMail.jsx
@@ -8,7 +8,7 @@ export const MainPageMail = () => {
     return(
         <div className="container-fluid p-5">
             <h1 className="display-4 fw-bold">Your mail box is ready to use!</h1>
-            <div className="container col-lg-6 d-flex flex-column m-4 border border-dark border-3 rounded-pill mx-auto">
+            <div className="container col-lg-6 d-flex flex-column m-4 border border-dark border-3 rounded mx-auto">
                 <i class="bi bi-person-circle icon-200"/>
                 <p className="fs-3 fw-bold">{userData.login}</p>
                 <p className="fs-3 fw-bold">Messages: {userData.messages.length}</p>
