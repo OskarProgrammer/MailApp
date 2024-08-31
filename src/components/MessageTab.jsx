0442e47f439a2f1925ff2fecd9e2ebb9829dbfd5
Oskar <Zaden>
2024-08-31 23:38:53 +0200

    Creating new component MessageTab.jsx

---
+++ src/components/MessageTab.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+export const MessageTab = (props) => {
+    const message = props.messageInfo
+
+    return (
+        <div className="container-fluid border border-1 border-dark p-3">
+            <h2 className="display-5 fw-bold">Subject: {message.subject}</h2>
+            <p className="display-6">To: {message.to}</p>
+            <button onClick={()=>{props.onDelete(message.id)}} className="btn btn-outline-danger btn-lg me-3">Remove</button>
+            <Link to={`/mail/${message.id}`} className="btn btn-outline-primary btn-lg">See details</Link>
+        </div>
+    )
+}
