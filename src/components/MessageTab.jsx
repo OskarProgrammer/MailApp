ae17e8c46d754bb7e4cd7d6876a22f064da549ec
Oskar <Zaden>
2024-09-01 13:05:21 +0200

    Changing text from 'Remove' to 'Put into the bin'

--- src/components/MessageTab.jsx
+++ src/components/MessageTab.jsx
@@ -7,7 +7,7 @@ export const MessageTab = (props) => {
         <div className="container-fluid border border-1 border-dark p-3">
             <h2 className="display-5 fw-bold">Subject: {message.subject}</h2>
             <p className="display-6"></p>
-            <button onClick={()=>{props.onDelete(message.id)}} className="btn btn-outline-danger btn-lg me-3">Remove</button>
+            <button onClick={()=>{props.onDelete(message.id)}} className="btn btn-outline-danger btn-lg me-3">Put into the bin</button>
             <Link to={`/mail/${message.id}`} className="btn btn-outline-primary btn-lg">See details</Link>
         </div>
     )
